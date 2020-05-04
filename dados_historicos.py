c98d31c2e6ab38b4822f90b6d05db43ecce271ef
lucasblazzi <lucasblazzi@hotmail.com>
2020-05-04 18:47:22 -0300

    testes prototipo

--- dados_historicos.py
+++ dados_historicos.py
@@ -1,7 +1,62 @@
 import requests
 import json
 import time
-#89272 - 2019 Embraer
+
+
+def busca_doc(ticker, ano):         #ATRIBUI O NUMERO SEQUENCIAL DOCUMENTO PARA CONSULTA B --> VALORES TESTE
+    doc = 666
+    print(ticker)
+    ano = int(ano)
+    if ticker == "MGLU3":
+        if ano == 2019:
+            doc = 88872
+        elif ano == 2017:
+            doc = 69380
+        elif ano == 2015:
+            doc = 51427
+
+    elif ticker == 'EMBR3':
+        if ano == 2019:
+            doc = 89272
+        elif ano == 2017:
+            doc = 69302
+        elif ano == 2015:
+            doc = 51106
+
+    elif ticker == 'CVCB3':
+        if ano == 2019:
+            doc = 89180
+        elif ano == 2017:
+            doc = 69574
+        elif ano == 2015:
+            doc = 51552
+
+    elif ticker == 'LREN3':
+        if ano == 2019:
+            doc = 88815
+        elif ano == 2017:
+            doc = 69238
+        elif ano == 2015:
+            doc = 51050
+
+    elif ticker == 'OIBR4':
+        if ano == 2019:
+            doc = 89882
+        elif ano == 2017:
+            doc = 69856
+        elif ano == 2015:
+            doc = 51559
+
+    elif ticker == 'CGAS5':
+        if ano == 2019:
+            doc = 89079
+        elif ano == 2017:
+            doc = 69534
+        elif ano == 2015:
+            doc = 51335
+    return doc
+
+
 
 def escreve_arquivo(ticker,ano,demon,dados):
     f = (open(ticker+'_'+ano+'_'+demon+'.json', "x"))
@@ -10,10 +65,11 @@ def escreve_arquivo(ticker,ano,demon,dados):
 
 
 
-def demonstrativo(token):                                                       #Request para iniciar o projeto
+def demonstrativo(token, doc):                                                                            #Request para iniciar o projeto
+    doc = str(doc)
     params = {
       "api_key": "t14QhaR-0UJq",
-      "start_url": "https://www.rad.cvm.gov.br/ENETCONSULTA/frmGerenciaPaginaFRE.aspx?NumeroSequencialDocumento=89272&CodigoTipoInstituicao=2",
+      "start_url": "https://www.rad.cvm.gov.br/ENETCONSULTA/frmGerenciaPaginaFRE.aspx?NumeroSequencialDocumento="+doc+"&CodigoTipoInstituicao=2",
     }
     r = requests.post('https://www.parsehub.com/api/v2/projects/'+token+'/run', data=params)
     print(r.text)
@@ -44,9 +100,9 @@ def main():
         ticker = input("Ticker: ")
         ano = input("Ano: ")
         demon = input("Demonstrativo: ")
-
         if (demon == 'dr' or demon == 'DR'):                                                     # tUgYN4_k8zKU = token do projeto DR
-            dados = demonstrativo('tUgYN4_k8zKU')
+            doc = busca_doc(ticker, ano)
+            dados = demonstrativo('tUgYN4_k8zKU', doc)
             aux = escreve_arquivo(ticker,ano,demon,dados)
             if aux == True:
                 print('Arquivo '+ticker+'_'+ano+'_'+demon+' gerado com sucesso!!!')
@@ -54,7 +110,8 @@ def main():
                 print('Problema ao gerar arquivo!!!')
 
         elif (demon == 'bpa' or demon == 'BPA'):                                 # tStJKRY4WWN_ = token do projeto BPA
-            dados = demonstrativo('tStJKRY4WWN_')
+            doc = busca_doc(ticker, ano)
+            dados = demonstrativo('tStJKRY4WWN_', doc)
             aux = escreve_arquivo(ticker,ano,demon,dados)
             if aux == True:
                 print('Arquivo '+ticker+'_'+ano+'_'+demon+' gerado com sucesso!!!')
@@ -62,7 +119,8 @@ def main():
                 print('Problema ao gerar arquivo!!!')
 
         elif (demon == 'bpp' or demon == 'BPP'):                                 # t3fX3x4OODkW = token do projeto BPP
-            dados =demonstrativo('t3fX3x4OODkW')
+            doc = busca_doc(ticker, ano)
+            dados = demonstrativo('t3fX3x4OODkW', doc)
             aux = escreve_arquivo(ticker,ano,demon,dados)
             if aux == True:
                 print('Arquivo '+ticker+'_'+ano+'_'+demon+' gerado com sucesso!!!')
