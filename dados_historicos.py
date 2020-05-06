49455a963c9e1618172a069d746dd904af51cf3a
lucasblazzi <lucasblazzi@hotmail.com>
2020-05-06 14:57:51 -0300

    Otimizacao

--- dados_historicos.py
+++ dados_historicos.py
@@ -115,81 +115,49 @@ def demonstrativo(token, doc):                                                  
 
 
 
-def modo1():                                                                    #Busca de um relatorio a partir de ticker + ano + demonstrativo
-    while True:
-        ticker = input("Ticker: ")
-        ano = input("Ano: ")
-        demon = input("Demonstrativo: ")
-        if demon == 'dr' or demon == 'DR':
-            doc = busca_doc(ticker, ano)
-            dados = demonstrativo('tUgYN4_k8zKU', doc)                          # tUgYN4_k8zKU = token do projeto DR
-            aux = escreve_arquivo(ticker,ano,demon,dados)
-            if aux == True:
-                print('Arquivo '+ticker+'_'+ano+'_'+demon+' gerado com sucesso!!!')
-            else:
-                print('Problema ao gerar arquivo!!!')
+def body(ticker, ano, demon, token):
+    doc = busca_doc(ticker, ano)
+    dados = demonstrativo(token, doc)
+    aux = escreve_arquivo(ticker,ano,demon,dados)
+    if aux == True:
+        print('Arquivo '+ticker+'_'+ano+'_'+demon+'.csv'' gerado com sucesso!!!')
+    else:
+        print("Falha na cópia do arquivo!!!")
 
-        elif demon == 'bpa' or demon == 'BPA':
-            doc = busca_doc(ticker, ano)
-            dados = demonstrativo('tStJKRY4WWN_', doc)                          # tStJKRY4WWN_ = token do projeto BPA
-            aux = escreve_arquivo(ticker,ano,demon,dados)
-            if aux == True:
-                print('Arquivo '+ticker+'_'+ano+'_'+demon+' gerado com sucesso!!!')
-            else:
-                print('Problema ao gerar arquivo!!!')
 
-        elif demon == 'bpp' or demon == 'BPP':
-            doc = busca_doc(ticker, ano)
-            dados = demonstrativo('t3fX3x4OODkW', doc)                          # t3fX3x4OODkW = token do projeto BPP
-            aux = escreve_arquivo(ticker,ano,demon,dados)
-            if aux == True:
-                print('Arquivo '+ticker+'_'+ano+'_'+demon+'.csv'' gerado com sucesso!!!')
-            else:
-                    print('Problema ao gerar arquivo!!!')
+def modo1(ticker, ano, demon):                                                                    #Busca de um relatorio a partir de ticker + ano + demonstrativo
+    if demon == 'dr' or demon == 'DR':
+        body(ticker, ano, demon, 'tUgYN4_k8zKU')        #gera DR
 
-        elif demon == 'dfc' or demon == 'DFC':
-            doc = busca_doc(ticker, ano)
-            dados = demonstrativo('tD1m6nCLOBRu', doc)                          # tD1m6nCLOBRu = token do projeto DFC
-            aux = escreve_arquivo(ticker,ano,demon,dados)
-            if aux == True:
-                print('Arquivo '+ticker+'_'+ano+'_'+demon+'.csv'' gerado com sucesso!!!')
-            else:
-                    print('Problema ao gerar arquivo!!!')
+    elif demon == 'bpa' or demon == 'BPA':
+        body(ticker, ano, demon, 'tStJKRY4WWN_')        #gera BPA
 
-        elif demon == 'sair':
-            break;
+    elif demon == 'bpp' or demon == 'BPP':
+        body(ticker, ano, demon, 't3fX3x4OODkW')        #gera BPP
 
-        else:
-            print('Demonstrativo nao encontrado...')
-            print('Digite: DR para Demonstracao dos Resultados')
-            print('Digite: BPA para Balanco Patrimonial Ativo')
-            print('Digite: BPP para Balanco Patrimonial Passivo')
-            print('Digite: sair para encerrar o programa')
-            continue;
+    elif demon == 'dfc' or demon == 'DFC':
+        body(ticker, ano, demon, 'tD1m6nCLOBRu')        #gera DFC
+
+    else:
+        print('Demonstrativo nao encontrado...')
+        print('Digite: DR para Demonstracao dos Resultados')
+        print('Digite: BPA para Balanco Patrimonial Ativo')
+        print('Digite: BPP para Balanco Patrimonial Passivo')
+        print('Digite: DFC para Demonstração de Fluxos de Caixa')
+        print('Digite: sair para encerrar o programa')
+        return
+
 
 def modo2(ticker):
     ano = 2015
     for ano in range(2015, 2020, 2):
         ano = str(ano)
-        doc = busca_doc(ticker, ano)
-        dados = demonstrativo('tUgYN4_k8zKU', doc)                          # tUgYN4_k8zKU = token do projeto DR
-        aux = escreve_arquivo(ticker,ano,'DR',dados)
-        print('Arquivo '+ticker+'_'+ano+'_'+'DR'+'.csv'' gerado com sucesso!!!')
+        body(ticker, ano, demon, 'tUgYN4_k8zKU')        #gera DR
+        body(ticker, ano, demon, 'tStJKRY4WWN_')        #gera BPA
+        body(ticker, ano, demon, 't3fX3x4OODkW')        #gera BPP
+        body(ticker, ano, demon, 'tD1m6nCLOBRu')        #gera DFC
 
-        doc = busca_doc(ticker, ano)
-        dados = demonstrativo('tStJKRY4WWN_', doc)                          # tStJKRY4WWN_ = token do projeto BPA
-        aux = escreve_arquivo(ticker,ano,'BPA',dados)
-        print('Arquivo '+ticker+'_'+ano+'_'+'BPA'+'.csv'' gerado com sucesso!!!')
 
-        doc = busca_doc(ticker, ano)
-        dados = demonstrativo('t3fX3x4OODkW', doc)                          # t3fX3x4OODkW = token do projeto BPP
-        aux = escreve_arquivo(ticker,ano,'BPP',dados)
-        print('Arquivo '+ticker+'_'+ano+'_'+'BPP'+'.csv'' gerado com sucesso!!!')
-
-        doc = busca_doc(ticker, ano)
-        dados = demonstrativo('tD1m6nCLOBRu', doc)                          # tD1m6nCLOBRu = token do projeto DFC
-        aux = escreve_arquivo(ticker,ano,'DFC',dados)
-        print('Arquivo '+ticker+'_'+ano+'_'+'BPP'+'.csv'' gerado com sucesso!!!')
 
 def main():
 
@@ -204,10 +172,16 @@ def main():
     print('2- Conjunto de demonstrativos (DR + BPA + BPP + DFC --> 6 anos)')
     modo = int(input('Opção:'))
     if modo == 1:
-        modo1()
+        while True:
+            ticker = input("Ticker: ")
+            ano = input("Ano: ")
+            demon = input("Demonstrativo: ")
+            modo1(ticker,ano,demon)
+
     elif modo == 2:
         ticker = input('Ticker:')
         modo2(ticker)
+
     else:
         print('Opção Invalida')
         exit()
